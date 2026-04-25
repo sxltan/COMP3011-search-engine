823d9d2887bca0d61e3ce5f096216907b41de1dd
Sultan <sc23s2a@leeds.ac.uk>
2026-04-26 01:30:54 +0200

    test: add mocked crawler tests for pagination and failures

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,4 +1,4 @@
-from src.crawler import extract_page_text, get_next_page_url
+from src.crawler import crawl_site, extract_page_text, get_next_page_url
 
 
 def test_extract_page_text_removes_script_and_style():
@@ -35,3 +35,51 @@ def test_get_next_page_url_returns_none_when_missing():
     next_url = get_next_page_url(html, "https://quotes.toscrape.com/page/10/")
 
     assert next_url is None
+
+
+def test_crawl_site_follows_pagination(monkeypatch):
+    first_page = """
+    <html>
+        <body>
+            <p>First page text</p>
+            <li class="next"><a href="/page/2/">Next</a></li>
+        </body>
+    </html>
+    """
+
+    second_page = """
+    <html>
+        <body>
+            <p>Second page text</p>
+        </body>
+    </html>
+    """
+
+    pages = {
+        "https://quotes.toscrape.com/": first_page,
+        "https://quotes.toscrape.com/page/2/": second_page,
+    }
+
+    def fake_fetch_page(url):
+        return pages[url]
+
+    monkeypatch.setattr("src.crawler.fetch_page", fake_fetch_page)
+
+    crawled_pages = crawl_site(delay=0)
+
+    assert len(crawled_pages) == 2
+    assert crawled_pages[0]["url"] == "https://quotes.toscrape.com/"
+    assert crawled_pages[1]["url"] == "https://quotes.toscrape.com/page/2/"
+    assert "First page text" in crawled_pages[0]["text"]
+    assert "Second page text" in crawled_pages[1]["text"]
+
+
+def test_crawl_site_stops_when_fetch_fails(monkeypatch):
+    def fake_fetch_page(url):
+        return None
+
+    monkeypatch.setattr("src.crawler.fetch_page", fake_fetch_page)
+
+    crawled_pages = crawl_site(delay=0)
+
+    assert crawled_pages == []
